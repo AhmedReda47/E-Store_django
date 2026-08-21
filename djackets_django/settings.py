3c178cc33e22eb3e280f85d175af41bf5417173a
Ahmed Reda <147887466+AhmedReda47@users.noreply.github.com>
2026-08-22 00:38:49 +0300

    Configure Cloudinary for media storage

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'cloudinary',
+    'cloudinary_storage',
+
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
@@ -146,3 +149,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
